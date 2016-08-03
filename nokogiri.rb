594778f8d02807c416472dda664005f0a1e2e955
Adri P <adri@adrimarie.us>
2016-08-03 00:35:57 -0700

    added sample nokogiri.rb file

---
+++ nokogiri.rb
@@ -0,0 +1,19 @@
+# We're gonna steal, I mean, scrap some stuff from the Martha Stewart website for sadnwiches. We're going to need the Ruby gem, Nokogiri, to scrap some sandwich recipes for us. Nokogiri ikou! (Let's go Nokogiri!)
+
+require 'nokogiri'
+require 'open-uri'
+
+# Scrappin' time!
+doc = Nokogiri::HTML(open(“http://www.marthastewart.com/312598/brick-pressed-sandwich”))
+
+# Check Chrome's Inspect for the element that lists down the ingredients and search.
+search = doc.css('div', 'section', 'ul')
+puts search
+
+# Class for the list of ingredients
+list = doc.css('.components-list')
+
+# looping
+list.each do |n|
+     puts n.inner_html
+end
